afd4765bd898a813ff91f1a600f56ede519b388f
xaz89 <dcg.jip@hotmail.com>
2019-01-31 13:55:36 -0600

    servicios

---
+++ index.php
@@ -0,0 +1,230 @@
+<!doctype html>
+<html lang="es">
+
+<head>
+  <title>LMS-index-Propuesta final</title>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/"
+    crossorigin="anonymous">
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="lms-theme.min.css">
+  <link rel="stylesheet" href="css/animate.css">
+</head>
+
+<body>
+  <div class="index  animated fadeIn faster">
+    <div class="frame">
+      <div class="redes text-white">
+        <a href="#" target="_blank"><i class="fab fa-facebook" alt="facebook" title="facebook"></i></a>
+        <a href="#" target="_blank"><i class="fab fa-twitter" alt="twitter" title="twitter"></i></a>
+        <a href="#" target="_blank"><i class="fab fa-whatsapp" alt="whatsapp" title="whatsapp"></i></a>
+      </div>
+      <div class="nada"></div>
+      <div class="menu">
+        <div class="pos-f-t">
+          <div class="collapse" id="navbarToggleExternalContent">
+            <div class="p-4">
+                <ul class="navbar-nav">
+                    <!-- <li class="nav-item active">
+                            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                          </li> -->
+                    <li class="nav-item col">
+                      <a class="nav-link" href="somos.html">Somos</a>
+                    </li>
+                    <li class="nav-item col">
+                      <a class="nav-link" href="mision.html">Misión</a>
+                    </li>
+                    <li class="nav-item col">
+                      <a class="nav-link" href="servicios.html">Servicios</a>
+                    </li>
+                    <li class="nav-item col">
+                      <a class="nav-link" href="contacto.html">Contacto</a>
+                    </li>
+                    <li class="nav-item col">
+                      <a class="nav-link" href="vacantes.html">Vacantes</a>
+                    </li>
+                  </ul>
+            </div>
+          </div>
+          <nav class="navbar navbar-dark">
+            <button class="fa-menu1" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent"
+              aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+              <!-- <span class="navbar-toggler-icon"></span> -->
+            </button>
+          </nav>
+        </div>
+      </div>
+      <div class="lms">
+      
+          <h1><span>L</span><span>M</span><span>S</span></h1> 
+          <h5>Loyalty Marketing Services</h5>
+          <!-- <div class="typewriter">
+              <p>Estrechamos relaciones</p>
+            </div> -->
+
+            <p>Estrechamos
+                <a href="" class="typewrite" data-period="500" data-type='[ "relaciones", "marketing", "diseño", "gamificación" ]'> 
+                  <span class="wrap" style="border-bottom: 1rem solid #fff;"></span>
+                </a>
+              </p>
+
+          <!-- <p>Estrechamos relaciones</p> -->
+
+          <!-- <div class="content">
+              <div class="wrapper">
+                  <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+                      x="0px" y="0px" viewBox="0 0 481.9 481.9" style="enable-background:new 0 0 481.9 481.9;"
+                      xml:space="preserve">
+                      <style type="text/css">
+                          .st0 {
+                              fill: #FFFFFF;
+                              stroke: #FFFFFF;
+                              stroke-miterlimit: 10;
+                          }
+                      </style>
+                      <g>
+                          <path class="st0" d="M142.6,226.5v-26.1c-2.7,0.7-5.3,1.8-7.8,3.5c-2.5,1.7-5.1,3.8-7.9,6.3c-2.8,2.5-4.5,3.8-5.2,3.8
+                                      c-0.8,0-1.2-1.1-1.2-3.2c0-2.1,2.2-5.9,6.6-11.4c4.4-5.4,9.3-10.5,14.6-15.1c5.3-4.6,9.2-6.9,11.5-6.9c1.4,0,2.6,0.8,3.7,2.4
+                                      c1,1.6,1.6,3.8,1.6,6.6c0,1.8,0,3.5-0.1,5.1c-0.1,1.6-0.1,3.6-0.2,6.2c-0.1,2.6-0.2,6.3-0.3,11.2c16.3-17.4,26-26.1,29-26.1
+                                      c0.9,0,1.6,0.4,2.2,1.1c0.6,0.7,0.8,1.6,0.8,2.6c0,2.4-2,6.2-5.9,11.2c-4,5.1-6.6,7.6-7.9,7.6c-1.8,0-3.3-0.3-4.3-0.9l-14.1,17.9
+                                      c0.1,6.5,0.7,11.3,1.7,14.5c1,3.2,2.8,4.8,5.4,4.8c2.2,0,4.8-1.1,7.8-3.3c3-2.2,7.8-6.3,14.4-12.3v7.6
+                                      C171.7,254.6,161,265,154.7,265c-3.9,0-6.7-2.1-8.4-6.2c-1.7-4.1-2.8-10.5-3.3-19.1c-2.6,2.7-5.6,5.9-9.1,9.6c-3.5,3.8-6,6.4-7.6,8
+                                      c-1.6,1.6-3,2.9-4.3,3.9c-1.3,1-2.5,1.6-3.5,1.6c-1,0-1.6-0.9-1.6-2.7c0-3,1.6-7,4.7-12.2c3.1-5.2,5.4-7.8,6.8-7.8
+                                      c0.1,0,0.7,0.1,1.8,0.4L142.6,226.5z" />
+                          <path class="st0" d="M200,206.6c11.2-10.2,22.1-17.8,32.7-22.7c10.6-4.9,18.7-7.4,24.5-7.4c1.6,0,3,0.6,4.3,1.9
+                                      c1.3,1.3,2.4,2.8,3.2,4.5c0.8,1.7,1.2,3,1.2,3.8c0,1.3-0.5,2.3-1.6,2.9c-1,0.6-2.8,1.4-5.4,2.3c0.5,1.2,1.2,2.7,2,4.5
+                                      c0.8,1.8,1.4,3.3,1.9,4.5c0.4,1.1,0.7,2.2,0.7,3c0,1.8-1.8,5.1-5.4,9.9c-3.6,4.8-6.1,8.4-7.5,10.7c-1.4,2.3-2.2,5.1-2.2,8.4
+                                      c0,5.5,2.5,8.2,7.5,8.2c5.2,0,13.3-4.8,24.3-14.5v6.8c-18.7,20.8-32.1,31.1-40.1,31.1c-2.9,0-4.9-0.9-6.1-2.8
+                                      c-1.2-1.9-1.8-4.7-1.8-8.4c0-5.8,2.1-14.3,6.3-25.4c-10.8,12-18.8,20.5-24,25.5c-5.2,5.1-9.6,7.6-13,7.6c-1.6,0-3.7-1.4-6.1-4.2
+                                      c-2.4-2.8-4.6-6.4-6.5-10.9c-1.9-4.5-2.8-9.1-2.8-13.9c0-3.5,0.3-6.4,1-8.7c0.7-2.3,2-4.7,3.9-7.1
+                                      C192.7,213.8,195.8,210.6,200,206.6z M253.1,194.4c-19.2,8.8-32.9,16.2-41,22.4c-8.2,6.1-12.2,12.4-12.2,18.9
+                                      c0,2.4,0.4,4.3,1.2,5.8c0.8,1.5,1.9,2.3,3.3,2.3c0.8,0,2.3-0.5,4.3-1.6c7.8-4.6,14.8-10.5,21.3-17.9
+                                      C236.3,217.1,244.1,207.1,253.1,194.4z" />
+                          <path class="st0" d="M277.8,236.2V228c4.1-4.3,8.2-10.3,12.5-17.9c-4.1-0.6-6.2-2.3-6.2-5c0-2.3,1.4-5.2,4.2-8.9
+                                      c2.8-3.6,6-6.9,9.7-9.7c3.7-2.9,6.7-4.3,8.8-4.3c2.4,0,3.7,1,3.7,3c0,0.8-0.2,1.3-0.7,1.6c-0.6,0.4-0.9,1-0.9,1.9
+                                      c0,0.7,0.6,1.1,1.8,1.1c3.7,0,8.3-0.8,13.9-2.3c5.6-1.5,9.4-2.3,11.5-2.3c1.7,0,2.8,1.6,3.4,4.8c1.6,8.4,2.3,13.2,2.3,14.3
+                                      c0,1-0.5,2-1.4,2.9c-0.9,0.9-3.5,2.8-7.8,5.5c-4.2,2.7-8.2,5.4-11.8,7.9c-3.6,2.5-8.3,6.1-14.1,10.7c4,1.8,6.9,3.9,8.7,6.2
+                                      c1.8,2.3,3.4,5.8,4.7,10.5l27.7-21.7v7.4L320,256.1c-0.7,7.1-3.2,14.3-7.4,21.6c-4.2,7.3-9,13.9-14.6,19.7
+                                      c-5.5,5.8-11,10.6-16.5,14.3c-5.5,3.7-9.5,5.6-12,5.6c-2.4,0-4.1-0.8-5.1-2.4c-1-1.6-1.4-3.9-1.4-6.9c0-4.4,0.9-8.5,2.8-12.4
+                                      c1.9-3.9,4.6-7.8,8.2-11.7c3.6-3.9,7.9-7.8,13-11.9c5.1-4,11.8-8.9,20.2-14.7c-0.8-5.2-2.2-8.5-4.1-9.8c-1.9-1.3-4.9-2.6-8.8-3.9
+                                      c-4-1.3-5.9-3.2-5.9-5.8c0-2.9,13.8-13.9,41.5-33.1c-4.4,0.9-9.6,1.9-15.6,2.9c-6,1.1-11.7,1.8-17.1,2.3c-3.8,6-7.3,11.2-10.7,15.7
+                                      C283.1,230.3,280.2,233.8,277.8,236.2z M306.1,267.9c-5.3,3.7-10.5,7.8-15.4,12.5c-4.9,4.6-9.2,9-12.8,13.1
+                                      c-3.6,4.1-5.4,6.6-5.4,7.5c0,1.7,0.7,2.5,2.2,2.5c2.7,0,6.1-1.5,10.3-4.4c4.2-3,8.3-7.2,12.3-12.7
+                                      C301.4,280.9,304.3,274.8,306.1,267.9z" />
+
+                      </g>
+                  </svg>
+
+              </div>
+
+          </div> -->
+          <!-- <div class="col">
+              <a class="btn btn-lms" href="somos.html">botón</a>
+          </div> -->
+      
+    
+  </div>
+      <div class="menu-inf">
+        <nav class="navbar navbar-expand-lg text-white">
+          <!-- <a class="navbar-brand" href="#">Menú</a> -->
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav"
+            aria-expanded="false" aria-label="Toggle navigation">
+          </button>
+          <div class="collapse navbar-collapse text-white" id="navbarNav">
+            <ul class="navbar-nav">
+              <!-- <li class="nav-item active">
+                      <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                    </li> -->
+              <li class="nav-item col">
+                <a class="nav-link" href="somos.html">Somos</a>
+              </li>
+              <li class="nav-item col">
+                <a class="nav-link" href="mision.html">Misión</a>
+              </li>
+              <li class="nav-item col">
+                <a class="nav-link" href="servicios.html">Servicios</a>
+              </li>
+              <li class="nav-item col">
+                <a class="nav-link" href="contacto.html">Contacto</a>
+              </li>
+              <li class="nav-item col">
+                <a class="nav-link" href="vacantes.html">Vacantes</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+    </div>
+    
+  </div>
+
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="js/jquery-3.3.1.slim.min.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+
+    <script>
+    var TxtType = function(el, toRotate, period) {
+        this.toRotate = toRotate;
+        this.el = el;
+        this.loopNum = 0;
+        this.period = parseInt(period, 1000) || 2000;
+        this.txt = '';
+        this.tick();
+        this.isDeleting = false;
+    };
+
+    TxtType.prototype.tick = function() {
+        var i = this.loopNum % this.toRotate.length;
+        var fullTxt = this.toRotate[i];
+
+        if (this.isDeleting) {
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+        } else {
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
+        }
+
+        this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+        var that = this;
+        var delta = 200 - Math.random() * 100;
+
+        if (this.isDeleting) { delta /= 2; }
+
+        if (!this.isDeleting && this.txt === fullTxt) {
+        delta = this.period;
+        this.isDeleting = true;
+        } else if (this.isDeleting && this.txt === '') {
+        this.isDeleting = false;
+        this.loopNum++;
+        delta = 500;
+        }
+
+        setTimeout(function() {
+        that.tick();
+        }, delta);
+    };
+
+    window.onload = function() {
+        var elements = document.getElementsByClassName('typewrite');
+        for (var i=0; i<elements.length; i++) {
+            var toRotate = elements[i].getAttribute('data-type');
+            var period = elements[i].getAttribute('data-period');
+            if (toRotate) {
+              new TxtType(elements[i], JSON.parse(toRotate), period);
+            }
+        }
+        // INJECT CSS
+        var css = document.createElement("style");
+        css.type = "text/css";
+        css.innerHTML = ".typewrite  .wrap { border-right: 0.03rem solid #fff;}";
+        document.body.appendChild(css);
+    };
+    </script>
+</body>
+
+</html>
