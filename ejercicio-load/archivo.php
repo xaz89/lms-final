f76ea775e5b000f2499648d96303ca630b3fb446
xaz89 <dcg.jip@hotmail.com>
2019-02-11 11:54:26 -0600

    analytics

--- ejercicio-load/archivo.php
+++ ejercicio-load/archivo.php
@@ -1,2 +1,4 @@
 <div id="contenido" style="background:blue;"><h1>Este es el nuevo texto ema</h1></div>
 <div id="no"><h2>Este es otro contenido</h2></div>
+
+<div id="titulo1"><h2>titulo1</h2></div>
