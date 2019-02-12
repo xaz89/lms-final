c5cbdaaa9dc2331cdf75b85c58ea351365a18945
xaz89 <dcg.jip@hotmail.com>
2019-02-12 12:29:27 -0600

    analytics-todos4

---
+++ archivo.php
@@ -0,0 +1,318 @@
+<!-- Modal planeacion-->
+<div class="modal1">
+    <div id="titulo1">
+        <h5 class="modal-title text-lms3">Planeación<br><span>estratégica</span></h5>
+    </div>
+    <div class="modal-imagen1">
+        <img src="img/fondo-planeacionOk.jpg" alt="" class="w-100">
+    </div>
+    <div class="modal-body1">
+        Definición de la estrategia del programa, así como los objetivos y metas a alcanzar en el corto, mediano y
+          largo plazo. Se evalúan, a su vez, los segmentos del mercado que deberán atacarse y sus respectivas etapas.
+          <br><br>
+          Se definen los siguientes conceptos:
+          <br><br>
+          <ul>
+            <li><span>Desarrollo de la estrategia del programa.</span></li>
+            <li><span>Conceptos para acumulación de puntos y redención de beneficios.</span></li>
+            <li><span>Análisis económico.</span></li>
+            <li><span>Estrategia de base de datos para mercadotecnia.</span></li>
+            <li><span>Desarrollo de investigación cualitativa.</span></li>
+            <li><span>Desarrollo de reglamento del programa.</span></li>
+            <li><span>Plan de lanzamiento.</span></li>
+            <li><span>Identificación de alianzas estratégicas potenciales.</span></li>
+          </ul>
+    </div>
+</div>
+
+  <!-- Modal d-sistemas-->
+<div class="modal2">
+    <div id="titulo2">
+        <h5 class="modal-title text-lms3">Desarrollo<br><span>de sistemas</span></h5>
+    </div>
+    <div class="modal-imagen2">
+        <img src="img/fondo-desarrolloOk.jpg" alt="" class="w-100">
+    </div>
+    <div class="modal-body2">
+    <ul>
+            <li><span>Consultoría: Estudios de reingeniería, diseño y auditoría de bases de datos, análisis y auditoría
+                de sistemas informáticos.</span></li>
+            <li><span>Desarrollo: Planeación, construcción, documentación y capacitación de sistemas de información.
+                Prestación de servicios especializados bajo la modalidad de personal asignado, en todos los niveles
+                para la construcción, mantenimiento, documentación y capacitación de sistemas de información.</span></li>
+            <li><span>Manipulación de bases de datos: Análisis, diseño e implantación de bases de datos: MS SQL Server
+                2005/2008, Oracle, MySQL y POSTGRES.</span></li>
+            <li><span>Desarrollo de aplicaciones Cliente/Servidor y Web utilizando las Plataformas Windows, LINUX y con
+                lenguajes de programación: C++, C SHARP, Visual Basic, Plataforma .NET y PHP.</span></li>
+            <li><span>Construcción de portales web.</span></li>
+            <li><span>Desarrollo de aplicaciones de Data Warehouse y Business Intelligence.</span></li>
+            <li><span>Desarrollo de aplicaciones con tarjeta inteligente (smart card) y lectores biométricos.</span></li>
+            <li><span>Desarrollo de aplicaciones móviles.</span></li>
+            <li><span>Desarrollo y adecuación de aplicaciones a las necesidades específicas de nuestros clientes.</span></li>
+            <li><span>Servicio de hospedaje de servidores y aplicaciones en ambiente web.</span></li>
+            <li><span>CMMI</span></li>
+
+          </ul>
+    </div>
+</div>
+
+
+  <!-- Modal soporte-->
+<div class="modal3">
+    <div id="titulo3">
+        <h5 class="modal-title text-lms3">Soporte<br><span>técnico</span></h5>
+    </div>
+    <div class="modal-imagen3">
+       <img src="img/fondo-soporteOk.jpg" alt="" class="w-100">
+    </div>
+    <div class="modal-body3">
+        <p>Servicio de recepción y atención de solicitudes para la solución de problemas.</p>
+        <p>Incluye la asistencia a usuarios en cuestiones técnicas relacionadas con la disponibilidad, desempeño y funcionalidad de las aplicaciones que dan soporte a los programas de nuestros clientes, así como al seguimiento de las solicitudes de solución de problemas.</p>
+        <p>El Help Desk brinda soporte dentro de los horarios hábiles de operación. Sin embargo, ofrecemos flexibilidad para implementar horarios de operación, de acuerdo con las necesidades y prioridades de nuestros clientes.</p>
+        <p>Tenemos disponible un teléfono de emergencia para responder a fallas mayores, las 24 horas del día y los 365 días del año.</p>
+        <p>Contamos con una aplicación para el seguimiento, escala y monitoreo de órdenes de trabajo (solución de problemas, sugerencias, servicio y quejas), que les permite a los agentes de Help Desk tener un control total sobre su operación, así como contar con teléfono, correo electrónico y chat.</p>
+    </div>
+</div>
+
+<!-- Modal aplicaciones-->
+<div class="modal4">
+    <div id="titulo4">
+        <h5 class="modal-title text-lms3">Aplicaciones<br><span>móviles</span></h5>
+    </div>
+    <div class="modal-imagen4">
+       <img src="img/fondo-appMovilesOk.jpg" alt="" class="w-100">
+    </div>
+    <div class="modal-body4">
+        <p>Desarrollamos aplicaciones móviles con enfoque en CRM para plataformas iOS, Android y Windows. </p>
+        <p>Soluciones dirigidas al cliente final, para que pueda consultar su información vía Apps y cuente con los beneficios que le brinda el programa en el que está inscrito.</p>
+        <p>Evaluación y seguimiento de planes de negocio, en conjunto con centros de consumo o puntos de venta.</p>
+        <p>Optimización de rutas y geolocalización de fuerza de ventas.</p>
+        <p>interaccion puntos de venta con programas de lealtad lms</p>
+    </div>
+</div>
+
+<!-- Modal promocion-->
+<div class="modal5">
+    <div id="titulo5">
+        <h5 class="modal-title text-lms3">Promoción y evaluación<br><span>de punto de venta</span></h5>
+    </div>
+    <div class="modal-imagen5">
+       <img src="img/fondo-promocionOk.jpg" alt="" class="w-100">
+    </div>
+    <div class="modal-body5">
+        <p>Servicio de evaluación de mercadeo y colocación de material POP con registro de operación en handheld, aplicación móvil e impresora portátil.</p>
+            <ul>
+              <li><span>Agenda</span></li>
+              <li><span>Registro de campo</span></li>
+              <li><span>Toma de evidencia</span></li>
+              <li><span>Geolocaclización</span></li>
+              <li><span>Levantamiento de encuestas</span></li>
+              <li><span>Productividad por ruta</span></li>
+              <li><span>Seguimiento de evaluadores</span></li>
+              <li><span>Progreso de iniciativas</span></li>
+              <li><span>Score card</span></li>
+            </ul>
+    </div>
+</div>
+
+<!-- Modal d-portales-->
+<div class="modal6">
+    <div id="titulo6">
+        <h5 class="modal-title text-lms3">Desarrollo<br><span>de portales</span></h5>
+    </div>
+    <div class="modal-imagen6">
+       <img src="img/fondo-portalesOk.jpg" alt="" class="w-100">
+    </div>
+    <div class="modal-body6">
+        <p>Entre nuestros servicios, contamos con el desarrollo de aplicaciones en línea: brindamos servicios integrales de desarrollo web de portales, hosting, dominios y herramientas de marketing.</p>
+        <p>Combinamos las mejores prácticas y una extensa experiencia en el desarrollo web de portales. Nuestro equipo de profesionales puede implementar en Internet un Portal Empresarial o un Portal Corporativo, a la medida exacta de las especificaciones de los clientes, adoptanto siempre las últimas tecnologías.</p>
+        <p>Utilizamos Diseño Responsive de páginas web, que permite adaptar la apariencia de las páginas web al dispositivo que se esté utilizando para visualizarla, como tabletas, teléfonos, equipos portátiles y PCs.</p>
+        <p>Posicionamos los sitios web de forma orgánica en Google, con el fin de llevarlos a los primeros resultados de Google y los demás buscadores, lo cual garantiza que aumente su ranking en visibilidad.</p>
+        <p>Siempre sabremos cuántas visitas ha tenido la página web, de dónde son las visitas y muchos datos más.</p>
+    </div>
+</div>
+
+<!-- Modal comunicacion-->
+<div class="modal7">
+    <div id="titulo7">
+        <h5 class="modal-title text-lms3">Comunicación<br><span>y diseño</span></h5>
+    </div>
+    <div class="modal-imagen7">
+       <img src="img/fondo-disenoOk.jpg" alt="" class="w-100">
+    </div>
+    <div class="modal-body7">
+        <p>Diseño y producción de documentos y piezas de comunicación electrónica e impresa:	</p>
+        <ul>
+          <li><span>Identidad gráfica del programa.</span></li>
+          <li><span>Eslogan y frases promocionales.</span></li>
+          <li><span>Página web.</span></li>
+          <li><span>Kit de bienvenida.</span></li>
+          <li><span>Formas administrativas: solicitud, contrato, estado de cuenta.</span></li>
+          <li><span>Folletos.</span></li>
+          <li><span>Posters.</span></li>
+          <li><span>Material punto de venta.</span></li>
+          <li><span>Newsletters.</span></li>
+          <li><span>Revistas.</span></li>
+          <li><span>Diseño y administración de campañas en redes sociales.</span></li>
+          <li><span>Diseño e implementación de aplicaciones móviles.</span></li>
+          <li><span>Diseño editorial.</span></li>
+        </ul>
+    </div>
+</div>
+
+<!-- Modal redes-->
+<div class="modal8">
+    <div id="titulo8">
+        <h5 class="modal-title text-lms3">Redes<br><span>sociales</span></h5>
+    </div>
+    <div class="modal-imagen8">
+       <img src="img/fondo-redesOk.jpg" alt="" class="w-100">
+    </div>
+    <div class="modal-body8">
+        <p>En LMS planificamos, diseñamos e implementamos campañas exitosas en redes sociales con el objetivo de adquirir una visión clara sobre la relación entre las marcas y sus clientes, cumplir con estrategias de mercadotecnia y crear un acercamiento entre nuestros clientes y los usuarios del programa.</p>
+        <ul>
+          <li><span>* Autentificación por medio de Facebook o Twitter.</span></li>
+          <li><span>* Campañas.</span></li>
+          <li><span>* Retos.</span></li>
+          <li><span>* Noticias.</span></li>
+          <li><span>* Trivias.</span></li>
+          <li><span>* Dashboard.</span></li>
+          <li><span>* Seguimiento de hashtags.</span></li>
+          <li><span>* Asignar puntos por participación e invitación.</span></li>
+          <li><span>* Ranking.</span></li>
+          <li><span>* Catálogo de recompensas.</span></li>
+        </ul>
+    </div>
+</div>
+
+
+<!-- Modal alianzas-->
+<div class="modal9">
+    <div id="titulo9">
+        <h5 class="modal-title text-lms3">Alianzas<br><span>estratégicas</span></h5>
+    </div>
+    <div class="modal-imagen9">
+       <img src="img/fondo-alianzasOk.jpg" alt="" class="w-100">
+    </div>
+    <div class="modal-body9">
+        <p>LMS establece alianzas estratégicas con empresas reconocidas, con la finalidad de brindar ventajas competitivas a sus clientes, quienes no podrían alcanzarlas de forma independiente en el corto plazo.</p>
+        <p>Ventajas: </p>
+        <ul>
+            <li><span>Sinergias que combinan lo mejor de ambas partes.</span></li>
+            <li><span>Más oportunidades al consolidar una oferta.</span></li>
+            <li><span>Acceso a nuevos mercados.</span></li>
+            <li><span>Incremento en la lealtad de clientes.</span></li>
+            <li><span>Contacto más directo con los clientes.</span></li>
+        </ul>
+    </div>
+</div>
+
+<!-- Modal call-->
+<div class="modal10">
+    <div id="titulo10">
+        <h5 class="modal-title text-lms3">Call<br><span>center</span></h5>
+    </div>
+    <div class="modal-imagen10">
+       <img src="img/fondo-alianzasOk.jpg" alt="" class="w-100">
+    </div>
+    <div class="modal-body10">
+        <p>En LMS ofrecemos soluciones completas para clientes con diversos giros y segmentos, a través de nuestros contact centers.</p>
+        <p>Servicios: </p>
+        <ul>
+          <li><span>Call Center activo y receptivo.</span></li>
+          <li><span>Contact Center.</span></li>
+          <li><span>Help Desk.</span></li>
+          <li><span>Cobranza.</span></li>
+          <li><span>Chat.</span></li>
+          <li><span>Recuperación de clientes.</span></li>
+          <li><span>Encuestas.</span></li>
+          <li><span>Ventas o reservaciones.</span></li>
+          <li><span>Agenda de visitas.</span></li>
+          <li><span>Atención post-venta.</span></li>
+        </ul>
+    </div>
+</div>
+
+<!-- Modal fulfillment-->
+<div class="modal11">
+    <div id="titulo11">
+        <h5 class="modal-title text-lms3">Fulfillment<br><span></span></h5>
+    </div>
+    <div class="modal-imagen11">
+       <img src="img/fondo-fullfilmentOk.jpg" alt="" class="w-100">
+    </div>
+    <div class="modal-body11">
+        <p>Impresión, personalización, doblado, ensobretado, enrutado y envío de comunicación impresa y electrónica.</p>
+        <ul>
+          <li><span>Correo directo.</span></li>
+          <li><span>Emisión de correo electrónico.</span></li>
+          <li><span>Envío de promocionales.</span></li>
+          <li><span>Newsletters.</span></li>
+          <li><span>Inserción de brochure.</span></li>
+          <li><span>Pick and pack.</span></li>
+          <li><span>Doblado y ensobretado.</span></li>
+          <li><span>Impresión láser.</span></li>
+        </ul>
+    </div>
+</div>
+
+<!-- Modal suministro-->
+<div class="modal12">
+    <div id="titulo12">
+        <h5 class="modal-title text-lms3">Suministro y<br><span>distribución<br>de premios</span></h5>
+    </div>
+    <div class="modal-imagen12">
+       <img src="img/fondo-suministroOk.jpg" alt="" class="w-100">
+    </div>
+    <div class="modal-body12">
+        <p>En LMS ofrecemos un servicio de suministro y distribución de premios que incluye:</p>
+        <p>Actividades:</p>
+        <ul>
+          <li><span>Selección.</span></li>
+          <li><span>Suministro.</span></li>
+          <li><span>Manejo y control de inventarios.</span></li>
+          <li><span>Rastreo de las entregas online.</span></li>
+          <li><span>Reportes de operación online.</span></li>
+        </ul>
+        <p>Catálogo:</p>
+        <p>Contamos con el más extenso catálogo de artículos, que se adecúa a las necesidades de nuestros clientes, en las siguientes categorías:</p>
+        <ul>
+          <li><span>Artículos para negocio.</span></li>
+          <li><span>Certificados de regalo.</span></li>
+          <li><span>Regalos de temporada.</span></li>
+          <li><span>Vacaciones.</span></li>
+          <li><span>Navidad.</span></li>
+          <li><span>Regreso a clases.</span></li>
+          <li><span>Electrodomésticos.</span></li>
+          <li><span>Electrónica.</span></li>
+          <li><span>Experiencias.</span></li>
+          <li><span>Herramientas.</span></li>
+          <li><span>Niños.</span></li>
+          <li><span>Personales.</span></li>
+          <li><span>Premium.</span></li>
+          <li><span>Tecnología.</span></li>
+          <li><span>Viajes.</span></li>
+        </ul> 
+    </div>
+</div>
+
+<!-- Modal gamification-->
+<div class="modal13">
+    <div id="titulo13">
+        <h5 class="modal-title text-lms3">Gamification<br><span></span></h5>
+    </div>
+    <div class="modal-imagen13">
+       <img src="img/fondo-gameOk.jpg" alt="" class="w-100">
+    </div>
+    <div class="modal-body13">
+        <p>Ayudamos a nuestros clientes a establecer relaciones fuertes y duraderas con sus consumidores, canales y empleados, al proporcionarles experiencias de interacción, que generen compromiso e involucramiento con la marca, aplicando conceptos y técnicas de juegos en nuestras estrategias de lealtad. </p>
+        <p>Con este enfoque, logramos generar interacciones emocionales positivas que nos ayudan a:</p>
+        <ul>
+          <li><span>Incrementar la participación en el sitio o punto de venta.</span></li>
+          <li><span>Enriquecimiento de la información y conocimiento social del cliente.</span></li>
+          <li><span>Incremento en redenciones.</span></li>
+          <li><span>Promoción de marca en redes sociales con efecto positivo «boca a boca».</span></li>
+        </ul>
+    </div>
+</div>
+
--- ejercicio-load/archivo.php
+++ ejercicio-load/archivo.php
@@ -1,4 +1,10 @@
-<div id="contenido" style="background:blue;"><h1>Este es el nuevo texto ema</h1></div>
+<div id="titulo1"><h1>Título1</h1></div>
 <div id="no"><h2>Este es otro contenido</h2></div>
+<div class="modal-body1"><img src="../img/alianzas.jpg" alt="" class="w-100"> Primer texto cambiado</div>
 
-<div id="titulo1"><h2>titulo1</h2></div>
+
+<div id="titulo2"><h1>Título2</h1></div>
+<div class="modal-body2"><img src="../img/alianzas.jpg" alt=""> Segundo texto cambiado</div>
+
+<div id="titulo3" class="text-white"><h1>Título3</h1></div>
+<div class="modal-body3"><img src="../img/amarillo.jpg" alt="" class="w-100"> Tercer texto cambiado</div>
